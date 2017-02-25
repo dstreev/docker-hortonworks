002cf303884264d3ab4ce34032390d3f59b6bb80
David W. Streever <david@streever.com>
2017-02-25 14:59:05 -0500

    First Docker Hub push.

--- build.sh
+++ build.sh
@@ -6,3 +6,6 @@ docker build --tag dstreev/ambari-server:2.4.2.0 ./ambari-server
 
 docker build --tag dstreev/ambari-agent:latest ./ambari-agent
 docker build --tag dstreev/ambari-server:latest ./ambari-server
+
+docker build --tag dstreev/nifi:2.1.1.0 ./nifi
+docker build --tag dstreev/nifi:latest ./nifi
