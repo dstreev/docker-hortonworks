fd7ab4483e654272deaf9807b1b1f162c677d2a4
David W. Streever <david@streever.com>
2017-03-09 11:50:38 -0800

    Centos6

--- push-images.sh
+++ push-images.sh
@@ -2,17 +2,17 @@
 
 . ./init.sh
 
-docker push dstreev/hdp-base:1.0
-docker push dstreev/hdp-base
+docker push dstreev/${OS_VERSION}_hdp-base:1.0
+docker push dstreev/${OS_VERSION}_hdp-base
 
-docker push dstreev/ambari-agent:${AMBARI_VERSION}
-docker push dstreev/ambari-agent
+docker push dstreev/${OS_VERSION}_ambari-agent:${AMBARI_VERSION}
+docker push dstreev/${OS_VERSION}_ambari-agent
 
-docker push dstreev/ambari-server:${AMBARI_VERSION}
-docker push dstreev/ambari-server
+docker push dstreev/${OS_VERSION}_ambari-server:${AMBARI_VERSION}
+docker push dstreev/${OS_VERSION}_ambari-server
 
 docker push dstreev/hdp-repo:1.0
 docker push dstreev/hdp-repo
 
-docker push dstreev/nifi:${NIFI_VERSION}
-docker push dstreev/nifi
+docker push dstreev/${OS_VERSION}_nifi:${NIFI_VERSION}
+docker push dstreev/${OS_VERSION}_nifi
