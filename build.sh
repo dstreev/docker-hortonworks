8a2d305fb58f1e239b2b0f25bd38a825a49a7f4c
David Streever <david@streever.com>
2018-04-03 16:01:49 -0400

    Added CentOS 6

--- build.sh
+++ build.sh
@@ -1,9 +1,26 @@
 #!/bin/bash
 
+echo "**** JDK-BASE *****"
+docker build --tag dstreev/centos7_jdk-base:latest ./jdk-base
+
+echo "**** IBM-BI-BASE *****"
+docker build --tag dstreev/centos7_ibm-bi-base:latest ../ibm_bi/base
+
 echo "**** HDP-BASE *****"
-docker build --tag dstreev/centos7_hdp-base ./hdp-base
+docker build --tag dstreev/centos7_hdp-base:latest ./hdp-base
 
 echo "**** AMBARI ****"
 docker build --tag dstreev/centos7_ambari:2 ./ambari
 docker build --tag dstreev/centos7_ambari:latest ./ambari
+
+echo "**** CentOS 6  *****"
+echo "**** JDK-BASE *****"
+docker build --tag dstreev/centos6_jdk-base:latest ./jdk-base-6
+
+echo "**** HDP-BASE(6) *****"
+docker build --tag dstreev/centos6_hdp-base:latest ./hdp-base-6
+
+echo "**** AMBARI (6) ****"
+docker build --tag dstreev/centos6_ambari:2 ./ambari-6
+docker build --tag dstreev/centos6_ambari:latest ./ambari-6
 
--- push-images.sh
+++ push-images.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
+docker push dstreev/centos7_jdk-base
+docker push dstreev/centos7_ibm-bi-base
+
 docker push dstreev/centos7_hdp-base
 
 docker push dstreev/centos7_ambari:2
 docker push dstreev/centos7_ambari:latest
+
+docker push dstreev/centos6_hdp-base:latest
+
+docker push dstreev/centos6_ambari:2
+docker push dstreev/centos6_ambari:latest
